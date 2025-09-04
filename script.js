ce4e57cdcbb72e60c769d1337b340b979dfb598e
2008carolc <00001107937061sp@al.educacao.sp.gov.br>
2025-09-04 14:59:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,19 @@ aumentaFonteBotao.addEventListener('click', function () {
  diminuiFonteBotao.addEventListener('click', function () {
  tamanhoAtualFonte -= 0.1;
  document.body.style.fontSize = \${tamanhoAtualFonte}rem``
-
+})const botaoDeAcessibilidade =
+document.getElementById('botaoacessibilidade');
+ const opcoesDeAcessibilidade =
+document.getElementById('opcoesacessibilidade');
+ botaoDeAcessibilidade.addEventListener('click', () => {
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+ })
+})
+alternaContraste.addEventListener('click', function(){
+ document.body.classList.toggle('alto-contraste');
  })
 
-})
+
+
+
